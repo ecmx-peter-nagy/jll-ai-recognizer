9297b677310e4db65d4761627f1a12b89c158856
Peter Nagy <peter.nagy@ecmxinternational.com>
2026-04-07 22:02:46 +0200

    Rebranded as gemini as JllAI

--- src/main/java/com/jll/gemini/model/FilePart.java
+++
@@ -1,9 +0,0 @@
-package com.jll.gemini.model;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record FilePart(
-    @JsonProperty("file_data") String fileData,
-    String filename,
-    @JsonProperty("mime_type") String mimeType
-) {}
